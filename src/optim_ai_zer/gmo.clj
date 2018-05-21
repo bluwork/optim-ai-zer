77fdcc5b36fd4a0d36c1fd27bec2c27dd7f992f9
bobanlukic <bbnlkc@gmail.com>
2018-05-21 13:25:08 +0200

    Gen individuals created

--- src/optim_ai_zer/gmo.clj
+++ src/optim_ai_zer/gmo.clj
@@ -1,1 +1,35 @@
-(ns optim-ai-zer.gmo)
+(ns ^{:author "BLu"
+      :doc "GMO - genetically modified objects"}
+    optim-ai-zer.gmo)
+
+(defn selection [population]
+  (fittest population))
+
+(defn crossover [])
+
+(defn mutation [population]
+  (let [mut-point (rand-int (:gene-length (first population)))]
+    ))
+
+(defn fittest
+  [population]
+  (loop [cnt (count population) max-fit 0]
+    (if (< cnt 1)
+      "finished"
+      (recur (dec cnt)))))
+
+(defn second-fittest
+  [])
+(selection )
+(take 10 (repeatedly #(rand-int 42)))
+
+(defn gen-individuals
+  "Param population size"
+  [pop-size genes-len]
+  (loop [pop-size pop-size individuals []]
+    (if (< pop-size 1)
+      individuals
+      (recur (dec pop-size) (conj individuals {:fitness 0 :genes (take genes-len (repeatedly #(rand-int 2)) )})))))
+(gen-individuals 3 10)
+
+(:genes (first (gen-individuals 3 10)))
