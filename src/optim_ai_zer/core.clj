7245a95982c037403e2f6ff6a7555d877b0c2af7
bobanlukic <bbnlkc@gmail.com>
2018-05-21 06:50:28 +0200

    Initial commit

---
+++ src/optim_ai_zer/core.clj
@@ -0,0 +1,7 @@
+(ns optim-ai-zer.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/optim_ai_zer/core_test.clj
@@ -0,0 +1,7 @@
+(ns optim-ai-zer.core-test
+  (:require [clojure.test :refer :all]
+            [optim-ai-zer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
